0c28aec3bcf249e92ff77723f1dc7c20359119cc
Trojanscript <64583763+addy005@users.noreply.github.com>
2021-02-08 10:15:19 +0530

    G

--- RDP.sh
+++ RDP.sh
@@ -27,7 +27,7 @@ su - Addy -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upate &> /dev/null
 then
-    printf "\n\nUpgrade Completed " >&2
+    printf "\n\nDo apt upgrade manually " >&2
 else
     printf "\n\nError Occured " >&2
 fi
