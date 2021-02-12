87e88c69e92877d8982d0f00da552856829ab425
Trojanscript <64583763+addy005@users.noreply.github.com>
2021-02-12 10:57:35 +0530

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -5,14 +5,20 @@ sudo useradd -m Addy
 sudo adduser Addy sudo
 echo 'Addy:9136' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+printf "\r$c$b    User Addy added $endc$enda\n" >&2 ||
+printf "\r$r$b    Error Occured $endc$enda\n" >&2
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
+printf "\r$c$b    Google Chrome Remote Installed $endc$enda\n" >&2 ||
+printf "\r$r$b    Error Occured $endc$enda\n" >&2
 sudo DEBIAN_FRONTEND=noninteractive \
 apt install --assume-yes xfce4 desktop-base
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
 sudo apt install --assume-yes xscreensaver
 sudo systemctl disable lightdm.service
+printf "\r$c$b    xfce installed $endc$enda\n" >&2 ||
+printf "\r$r$b    Error Occured $endc$enda\n" >&2
 sudo apt install nautilus nano -y 
 sudo adduser Addy chrome-remote-desktop
 } &> /dev/null &&
